1435a048ea565d11ddb146ebf298d04ba5f65621
BotMaster3000 <jdgebauer@gmx.de>
2018-12-28 21:18:26 +0100

    Added 3 UnitTests, implemented CountingSort

---
+++ SortingAlgorithms/SortingAlgorithms/Algorithms/CountingSort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortingAlgorithms.Interfaces;
+
+namespace SortingAlgorithms.Algorithms
+{
+    public class CountingSort : ISortingAlgorithm
+    {
+        private Dictionary<int, int> unsortedDictionary;
+
+        public int[] Sort(int[] unsortedArray)
+        {
+            CountOccurances(unsortedArray);
+
+            int[] dictionaryKeys = SortDictionaryKeys();
+
+            return GetReturnArray(dictionaryKeys);
+        }
+
+        private Dictionary<int, int> CountOccurances(int[] unsortedArray)
+        {
+            unsortedDictionary = new Dictionary<int, int>();
+            foreach (int number in unsortedArray)
+            {
+                if (unsortedDictionary.Keys.Contains(number))
+                {
+                    ++unsortedDictionary[number];
+                }
+                else
+                {
+                    unsortedDictionary.Add(number, 1);
+                }
+            }
+            return unsortedDictionary;
+        }
+
+        private int[] SortDictionaryKeys()
+        {
+            int[] dictionaryKeys = unsortedDictionary.Keys.ToArray();
+            for (int i = 0; i < dictionaryKeys.Length; ++i)
+            {
+                for (int j = i + 1; j < dictionaryKeys.Length; ++j)
+                {
+                    if (dictionaryKeys[i] > dictionaryKeys[j])
+                    {
+                        int tempInt = dictionaryKeys[i];
+                        dictionaryKeys[i] = dictionaryKeys[j];
+                        dictionaryKeys[j] = tempInt;
+                    }
+                }
+            }
+            return dictionaryKeys;
+        }
+
+        private int[] GetReturnArray(int[] dictionaryKeys)
+        {
+            List<int> returnList = new List<int>();
+            foreach (int key in dictionaryKeys)
+            {
+                for (int i = 0; i < unsortedDictionary[key]; ++i)
+                {
+                    returnList.Add(key);
+                }
+            }
+            return returnList.ToArray();
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithmsTests/Algorithms/CountingSortTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAlgorithms.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms.Algorithms.Tests
+{
+    [TestClass]
+    public class CountingSortTests
+    {
+        private void CheckIfInOrder(int[] numberArray)
+        {
+            for (int i = 1; i < numberArray.Length; ++i)
+            {
+                if (numberArray[i - 1] > numberArray[i])
+                {
+                    Assert.Fail($"Array not in order. {numberArray[i - 1]} is bigger than {numberArray[i]}");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SortTest_SingleOccurance_NoGap()
+        {
+            int[] numberArray = new int[] { 4, 2, 3, 1, 6, 0, 5, 7, 9, 8 };
+            CountingSort sorter = new CountingSort();
+            int[] returnArray = sorter.Sort(numberArray);
+            CheckIfInOrder(returnArray);
+        }
+
+        [TestMethod]
+        public void SortTest_SingleOccurance_Gap()
+        {
+            int[] numberArray = new int[] { 4, 2, 9, 19, 500 };
+            CountingSort sorter = new CountingSort();
+            int[] returnArray = sorter.Sort(numberArray);
+            CheckIfInOrder(returnArray);
+        }
+
+        [TestMethod]
+        public void SortTest_MultipleOccurance_Gap()
+        {
+            int[] numberArray = new int[] { 5, 5, 10, 10, 7, 8, 20, 1, 0 };
+            CountingSort sorter = new CountingSort();
+            int[] returnArray = sorter.Sort(numberArray);
+            CheckIfInOrder(returnArray);
+        }
+    }
+}
