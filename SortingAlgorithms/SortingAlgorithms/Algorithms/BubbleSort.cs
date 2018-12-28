2c9934389a4a3c9e1404c6b7387c2e18673b928e
BotMaster3000 <jdgebauer@gmx.de>
2018-12-28 20:08:42 +0100

    Created BubbleSort-Class with implements ISortingAlgorithm and Created a UnitTest-Project

---
+++ SortingAlgorithms/SortingAlgorithms/Algorithms/BubbleSort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortingAlgorithms.Interfaces;
+
+namespace SortingAlgorithms.Algorithms
+{
+    public class BubbleSort : ISortingAlgorithm
+    {
+        public int[] Sort(int[] unsortedArray)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithmsTests/Algorithms/BubbleSortTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAlgorithms.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms.Algorithms.Tests
+{
+    [TestClass]
+    public class BubbleSortTests
+    {
+        [TestMethod]
+        public void SortTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
