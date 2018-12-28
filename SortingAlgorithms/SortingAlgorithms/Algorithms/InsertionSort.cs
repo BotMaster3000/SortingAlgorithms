1508e62d91efc664866957be74c66b5f283bd118
BotMaster3000 <jdgebauer@gmx.de>
2018-12-28 20:44:02 +0100

    Added 3 UnitTests and implemented InsertionSort

---
+++ SortingAlgorithms/SortingAlgorithms/Algorithms/InsertionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortingAlgorithms.Interfaces;
+
+namespace SortingAlgorithms.Algorithms
+{
+    public class InsertionSort : ISortingAlgorithm
+    {
+        public int[] Sort(int[] unsortedArray)
+        {
+            for(int i = 0; i < unsortedArray.Length; ++i)
+            {
+                for(int j = i + 1; j < unsortedArray.Length; ++j)
+                {
+                    if(unsortedArray[i] > unsortedArray[j])
+                    {
+                        int tempInt = unsortedArray[i];
+                        unsortedArray[i] = unsortedArray[j];
+                        unsortedArray[j] = tempInt;
+                    }
+                }
+            }
+            return unsortedArray;
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithmsTests/Algorithms/InsertionSortTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortingAlgorithms.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms.Algorithms.Tests
+{
+    [TestClass]
+    public class InsertionSortTests
+    {
+        private void CheckIfInOrder(int[] numberArray)
+        {
+            for (int i = 1; i < numberArray.Length; ++i)
+            {
+                if (numberArray[i - 1] > numberArray[i])
+                {
+                    Assert.Fail($"Array not in order. {numberArray[i - 1]} is bigger than {numberArray[i]}");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SortTest_SingleOccurance_NoGap()
+        {
+            int[] numberArray = new int[] { 4, 2, 3, 1, 6, 0, 5, 7, 9, 8 };
+            InsertionSort sorter = new InsertionSort();
+            sorter.Sort(numberArray);
+            CheckIfInOrder(numberArray);
+        }
+
+        [TestMethod]
+        public void SortTest_SingleOccurance_Gap()
+        {
+            int[] numberArray = new int[] { 4, 2, 9, 19, 500 };
+            InsertionSort sorter = new InsertionSort();
+            sorter.Sort(numberArray);
+            CheckIfInOrder(numberArray);
+        }
+
+        [TestMethod]
+        public void SortTest_MultipleOccurance_Gap()
+        {
+            int[] numberArray = new int[] { 5, 5, 10, 10, 7, 8, 20, 1, 0 };
+            InsertionSort sorter = new InsertionSort();
+            sorter.Sort(numberArray);
+            CheckIfInOrder(numberArray);
+        }
+    }
+}
